7d3a9c4a81d9e476804788c80d7d2faa257d1866
Kevin Thomas <thomas.kevin@ufl.edu>
2019-03-11 15:41:16 -0400

    Adding Bootcamp4, no integration

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+//This file holds any configuration variables we may need 
+//'config.js' is typically ignored by git to protect sensitive information, such as your database's username and password
+
+module.exports = {
+  db: {
+    uri: 'mongodb://bootcamp:acc3ss@ds051720.mlab.com:51720/bootcamp3', //place the URI of your mongo database here.
+  }, 
+  port: 5000
+};
