928d9e3c089193b478138adcf7a9b4618309698a
IgnisCyan <bwclark97@gmail.com>
2019-03-31 19:13:29 -0400

    Added search controller

---
+++ server/controllers/search.server.controller.js
@@ -0,0 +1,14 @@
+var Twit = requires('twit')
+var config = requires('./config/config.js')
+var T = new Twit({
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    access_token: config.access_token,
+    access_token_secret: config.access_token_secret,
+})
+
+function getTweetsFromQuery(query){
+    T.Get('search/tweets', {q:query, count: 100}, function(err, data, response){
+        console.log(data); 
+    })
+}
