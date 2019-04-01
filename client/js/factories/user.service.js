7b98d5afe7d7e477525a6ae15f65e1f042b83bb5
Kevin Thomas <thomas.kevin@ufl.edu>
2019-04-01 05:44:24 -0400

    Heroku deployment modifications made

--- client/js/factories/user.service.js
+++ client/js/factories/user.service.js
@@ -17,26 +17,26 @@
 
         return service;
         function GetCurrent() {
-            return $http.get('/api/users/current').then(handleSuccess, handleError);
+            return $http.get('https://vast-scrubland-38554.herokuapp.com/api/users/current').then(handleSuccess, handleError);
         }
         function GetById(_id) {
-            return $http.get('/api/users/' + _id).then(handleSuccess, handleError);
+            return $http.get('https://vast-scrubland-38554.herokuapp.com/api/users/' + _id).then(handleSuccess, handleError);
         }
         function GetAll() {
-            return $http.get('/api/users').then(handleSuccess, handleError);
+            return $http.get('https://vast-scrubland-38554.herokuapp.com/api/users').then(handleSuccess, handleError);
         }
 
         function GetByUsername(username) {
-            return $http.get('/api/users/' + username).then(handleSuccess, handleError);
+            return $http.get('https://vast-scrubland-38554.herokuapp.com/api/users/' + username).then(handleSuccess, handleError);
         }
         function Create(user) {
-            return $http.post('/api/users', user).then(handleSuccess, handleError);
+            return $http.post('https://vast-scrubland-38554.herokuapp.com/api/users', user).then(handleSuccess, handleError);
         }
         function Update(user) {
-            return $http.put('/api/users/' + user._id, user).then(handleSuccess, handleError);
+            return $http.put('https://vast-scrubland-38554.herokuapp.com/api/users/' + user._id, user).then(handleSuccess, handleError);
         }
         function Delete(_id) {
-            return $http.delete('/api/users/' + _id).then(handleSuccess, handleError);
+            return $http.delete('https://vast-scrubland-38554.herokuapp.com/api/users/' + _id).then(handleSuccess, handleError);
         }
         function handleSuccess(res) {
             return res.data;
