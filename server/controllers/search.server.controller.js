68dfb9e2445246a9904c84073beb04f943460395
boydDuffie <shmeeves@ufl.edu>
2019-04-15 20:42:12 -0400

    changed variable names back to HG standard for debugging.

--- server/controllers/search.server.controller.js
+++ server/controllers/search.server.controller.js
@@ -2,17 +2,17 @@
 
 var Twitter = require('twitter');
 
-var chirp;
+var twitter_response;
 
 var client = new Twitter({
     consumer_key: '0Ce89vEhtDVJRrznQRs13kVnQ';
     consumer_secret: 'RceoBEhRvmGWioysz5PrDDGgv1OhKlITmbKkjEIpJZTxc6BZZM';
-    access_token: '1070800768230400001-S7sB9x9emGcGJeNPQHOp6xaCUxLqMl';
+    access_token_key: '1070800768230400001-S7sB9x9emGcGJeNPQHOp6xaCUxLqMl';
     access_token_secret: 'V7djS4GQmrl3d2soJEm0T30KSev6bpP15i7fJ4dFemNuF';
 });
 
 //geocoder api
-var ng = require('node-geocoder');
+var NodeGeocoder = require('node-geocoder');
 
 var options = {
     provider: 'opencage',
@@ -21,20 +21,20 @@ var options = {
     formatter: null
 };
 
-var geo = ng(options);
+var geocoder = NodeGeocoder(options);
 
-function getwoeid(input) {
+function checkValidArea(userInput) {
     let promise = new Promise(function (resolve, reject) {
-        client.get('trends/available', function (err, res) {
+        client.get('trends/available', function (error, response) {
             if(err){
                 console.log(err);
                 reject(err);
                 throw err;
             }
-            for (let i=0; i<res.length; i++){
-                if(res[i].name == userInput) {
-                    console.log("woeid: " = res[i].woeid);
-                    resolve(res[i].woeid);
+            for (let i=0; i<response.length; i++){
+                if(response[i].name == userInput) {
+                    console.log("woeid: " = response[i].woeid);
+                    resolve(response[i].woeid);
                 }
             };
             resolve(-1);
@@ -43,7 +43,7 @@ function getwoeid(input) {
     return promise;
 }
 
-function sort(criteria){
+function sorting(criteria){
     return function (a, b){
         if(a[criteria] < b[criteria]) {
             return 1;
@@ -56,18 +56,17 @@ function sort(criteria){
 }
 
 
-exports.trendsByLoc = function (req, res, next) {
-    getwoeid(req.params.userPlace).then(woeid => {
-        console.log(woeid);
+exports.dynamicTrends = function (req, res, next) {
+    checkValidArea(req.params.userPlace).then(woeid => {
         if (woeid != -1) {
             client.get('trends/place', {id: woeid}, function(err, resp) {
                 if (err) {
                     console.log(err);
                     throw err;
                 }
-                chirp = resp;
-                chirp[0].trends.sort(sort("tweet_volume"));
-                return res.status(200).json(chirp);
+                twitter_response = response;
+                twitter_response[0].trends.sort(sorting("tweet_volume"));
+                return res.status(200).json(twitter_response);
             });
         }
         else {
@@ -76,53 +75,77 @@ exports.trendsByLoc = function (req, res, next) {
     });
 }
 
-function latLong(area) {
+function getCoord(area) {
     let promise = new Promise(function (resolve, reject) {
-        geo.geocode(area, function(err, res) {
-            if(err){
-                console.log(err);
+        geocoder.geocode(place, function(error, response) {
+            if(error){
+                console.log(error);
                 resolve(-1);
             }
             var lat = res[0].latitude,
-                long = res[0].longitude,
-                pair = {
+                lon = res[0].longitude,
+                coord = {
                     latitude: lat,
-                    longitude: long
+                    longitude: lon
                 };
-                resolve(pair);
+                resolve(coord);
         });
     })
     return promise;
 }
 
-function hashtag(topic2, hash){
+exports.areaTopicTweets = function(req, res, next){
+    var topic = req.params.topic;
+    var place = req.params.place;
+    var isHash = req.params.isHash;
+
+    if(isHash === "true"){
+        topic = "#" + topic;
+    }
+
+    getCoord(place).then(function(coord){
+        var searchLocation = coord.latitude+','+coord.longitude+',10mi';
+        var searchQuery = " -RT " + topic;
+        client.get('search/tweets', {q: searchQuery, geocode: searchLocation}, function(error, response){
+            if(error){
+                return res.status(200).json("couldn't get tweet data about trend in location");
+            }
+            if(response.statuses.length==0){
+                return res.status(200).json("no tweets here");
+            }
+            return res.status(200).json(response);
+        });
+    });
+}
+
+function hashCheck(newTopic, hash){
     let promise = new Promise(function(resolve, reject){
         if(hash === "true"){
-            topic2 = "#" + topic2;
+            newTopic = "#" + newTopic;
         }
-        resolve(topic2);
+        resolve(newTopic);
     })
     return promise;
 }
 
-exports.tweetsByQ = function(req, res, next){
+exports.topicTweets = function(req, res, next){
     var topic = req.params.topic;
     var isHash = req.params.isHash;
 
-    hashtag(topic, isHash).then(function(topic2){
-        console.log(topic2);
-        var Q = " -RT " + topic2;
-        console.log(Q);
-        client.get('search/tweets', {q: Q, result_type: 'popular'}, function(err, resp){
-            if(err){
-                console.log(err);
+    hashCheck(topic, isHash).then(function(newTopic){
+        console.log(newTopic);
+        var searchQuery = " -RT " + newTopic;
+        console.log(searchQuery);
+        client.get('search/tweets', {q: searchQuery, result_type: 'popular'}, function(error, response){
+            if(error){
+                console.log(error);
                 return res.status(200).json("failed to get tweet data");
             }
-            if(resp.statuses.length == 0){
+            if(response.statuses.length == 0){
                 console.log("nothing here");
                 return res.status(200).json("nothing here");
             }
-            return res.status(200).json(resp);
+            return res.status(200).json(response);
         });
     });
 }
